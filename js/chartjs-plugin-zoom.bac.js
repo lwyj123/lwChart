6336c591bfd0507f0f3ed4f37361ce8b81a9faf7
Yuyi Liang <Yuyi Liang>
2016-12-05 09:45:41 +0800

    deleted animation part

--- js/chartjs-plugin-zoom.bac.js
+++ js/chartjs-plugin-zoom.bac.js
@@ -1,3 +1,15 @@
+/*!
+ * chartjs-plugin-zoom
+ * http://chartjs.org/
+ * Version: 0.4.3
+ *
+ * Copyright 2016 Evert Timberg
+ * Released under the MIT license
+ * https://github.com/chartjs/chartjs-plugin-zoom/blob/master/LICENSE.md
+ */
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+},{}],2:[function(require,module,exports){
 /*jslint browser:true, devel:true, white:true, vars:true */
 /*global require*/
 
@@ -471,3 +483,5 @@ var zoomPlugin = {
 
 module.exports = zoomPlugin;
 Chart.pluginService.register(zoomPlugin);
+
+},{"chart.js":1,"hammerjs":1}]},{},[2]);
